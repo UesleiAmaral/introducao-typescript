f99d7f0780456cca778217b5fed3793b2602d8e9
Ueslei Amaral <uespamaral@gmail.com>
2022-06-06 14:11:41 -0300

    exercicios introdução ao typescript

---
+++ src/app.ts
@@ -0,0 +1,86 @@
+export{}
+
+// RESPOSTA DESAFIO 1;
+const employee = {
+    codigo: 10,
+    nome: 'João'
+};
+employee.codigo = 10;
+employee.nome = "John";
+
+
+
+// RESPOSTA DESAFIO 2;
+
+enum Trabalho {
+    Atriz,
+    Padeiro
+} 
+
+type Humano = {
+    nome: string,
+    idade: number,
+    profissao: Trabalho
+}
+
+let pessoa1: Humano = {
+    nome: 'maria',
+    idade: 29,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa2: Humano = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: Trabalho.Padeiro
+};
+
+let pessoa3: Humano = {
+    nome: 'laura',
+    idade: 32,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa4: Humano = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Trabalho.Padeiro
+}
+
+
+// RESPOSTA DESAFIO 3;
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
+
+let saldoTotal = 0
+
+limparSaldo()
+
+function somarAoSaldo(soma: number) {
+    if (campoSaldo) {
+        saldoTotal += soma
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+
+function limparCampoSoma() {
+    soma.value = "";
+}
+
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', () => {
+        somarAoSaldo(Number(soma.value)); 
+    });
+}
+botaoLimpar.addEventListener('click', () => { limparSaldo();});
